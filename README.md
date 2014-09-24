5fd15c308fd36ae4e949a33fa29aba29dc3968a2
Rafael Capdevielle <capdevielle@gmail.com>
2014-09-24 17:26:52 -0300

    Better README. Indentation for SerialCommands.h

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 qcopter
 =======
 
+Open Source code for a Quad copter based on Arduino.
+
 Requirements:
 -------------
 
 From https://github.com/jrowberg/i2cdevlib
 add to your include file or libraries:
+ 
  * i2cdev
  * MPU6050
--- SerialCommands.h
+++ SerialCommands.h
@@ -6,69 +6,66 @@
 
 // Really basic parser for commands
 void select_command(String command, String value){
-  if(command == "motors0"){
-    setMotor(0, value.toInt());
-  }else if(command == "motors1"){
-    setMotor(1, value.toInt());
-  }else if(command == "motors2"){
-    setMotor(2, value.toInt());
-  }else if(command == "motors3"){
-    setMotor(3, value.toInt());
-  }else{
-    Serial.println("Command not found");
-  }
+    if(command == "stop"){
+        Serial.println("stop!");    
+    }else if(command == "acc"){
+        Serial.print("acc:");
+        Serial.println(value.toInt());    
+    }else{
+        Serial.println("Command not found");
+    }
 }
 
 // here to process incoming serial data after a terminator received
 void process_data (const char * data){
-  String in = String(data);
-  String command, value;
-  int token = in.indexOf(':');
+    String in = String(data);
+    String command, value;
+    int token = in.indexOf(':');
 
-  if(token >= 0){
-    command = in.substring(0,token);
-    value = in.substring(token+1);
+    if(token >= 0){
+        command = in.substring(0,token);
+        value = in.substring(token+1);
+    }else{
+        command = in;
+        value = "";
+    }
     select_command(command, value);
-  }
-}  // end of process_data
+}
 
 void processIncomingByte (const byte inByte){
-  static char input_line[MAX_INPUT];
-  static unsigned int input_pos = 0;
+    static char input_line[MAX_INPUT];
+    static unsigned int input_pos = 0;
 
-  switch (inByte){
+    switch (inByte){
 
-    case ';':   // end of textx
-      input_line [input_pos] = 0;  // terminating null byte
+        case ';':     // end of textx
+            input_line [input_pos] = 0;    // terminating null byte
 
-      // terminator reached! process input_line here ...
-      process_data (input_line);
+            // terminator reached! process input_line here ...
+            process_data (input_line);
 
-      // reset buffer for next time
-      input_pos = 0;
-      break;
+            // reset buffer for next time
+            input_pos = 0;
+            break;
 
-    case '\n':   // discard newline
-      break;
-    case '\r':   // discard carriage return
-      break;
+        case '\n':     // discard newline
+            break;
+        case '\r':     // discard carriage return
+            break;
 
-    default:
-      // keep adding if not full ... allow for terminating null byte
-      if (input_pos < (MAX_INPUT - 1))
-        input_line [input_pos++] = inByte;
-      break;
-
-    }  // end of switch
-
-  } // end of processIncomingByte
+        default:
+            // keep adding if not full ... allow for terminating null byte
+            if (input_pos < (MAX_INPUT - 1))
+                input_line [input_pos++] = inByte;
+            break;
+    }
+}
 
 void readSerialCommands(){
-  // if serial data available, process it
-  if (Serial.available () > 0)
-    processIncomingByte (Serial.read());
-
-  // do other stuff here like testing digital input (button presses) ...
-}  // end of loop
+    // if serial data available, process it
+    if (Serial.available () > 0){
+        processIncomingByte (Serial.read());
+    }
+}
 
 #endif
